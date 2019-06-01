ccfd9c33016f9e49455549e4f16ed360d9c47474
JasonLee <1615685977@qq.com>
2019-06-01 14:41:33 +0800

    Site updated: 2019-06-01 14:41:16

---
+++ music/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  autoplay: false,
+  audio: [
+    {
+      name: "Es ware schon gewesen (It Could Have Been)",
+      artist: 'Ulli Bögershausen',
+      url: 'http://music.163.com/song/media/outer/url?id=2065349.mp3 ',
+      cover: 'http://p1.music.126.net/GrBJUAYcfgwbaZywULYwMA==/931286348781408.jpg',
+    }
+  ]
+});
