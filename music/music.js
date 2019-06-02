0136873cbc51f1869d4b335e265a284e368be5f2
JasonLee <1615685977@qq.com>
2019-06-02 15:47:47 +0800

    Site updated: 2019-06-02 15:47:23

--- music/music.js
+++ music/music.js
@@ -4,10 +4,30 @@ const ap = new APlayer({
   autoplay: false,
   audio: [
     {
-      name: "Es ware schon gewesen (It Could Have Been)",
+      name: "Es ware schon gewesen",
       artist: 'Ulli Bögershausen',
-      url: 'http://music.163.com/song/media/outer/url?id=2065349.mp3 ',
+      url: 'http://music.163.com/song/media/outer/url?id=2065349.mp3',
       cover: 'http://p1.music.126.net/GrBJUAYcfgwbaZywULYwMA==/931286348781408.jpg',
-    }
+    },{
+      name: "So Cute~",
+      artist: 'Lopu$',
+      url: 'http://music.163.com/song/media/outer/url?id=433107530.mp3',
+      cover: 'http://p1.music.126.net/yFCpD3GrgmcInAbVvVaFUg==/17999005346907556.jpg',
+    },{
+      name: "Casablanca",
+      artist: 'Bertie Higgins',
+      url: 'http://music.163.com/song/media/outer/url?id=5134039.mp3',
+      cover: 'http://p1.music.126.net/Ny5AEURZGTbeJBKNS9TflQ==/1658063534698769.jpg',
+    },{
+      name: "Come and Get Your Love",
+      artist: 'Redbone',
+      url: 'http://music.163.com/song/media/outer/url?id=28864241.mp3',
+      cover: 'http://p2.music.126.net/5i5SKVW_F1ub2BgDeyjI5A==/3225967119049341.jpg',
+    },{
+      name: "Hotel California",
+      artist: 'Eagles',
+      url: 'http://music.163.com/song/media/outer/url?id=26289183.mp3',
+      cover: 'http://p2.music.126.net/AgCNb4uf4kFwn-cccBf_Nw==/109951164017277247.jpg',
+    },
   ]
 });
